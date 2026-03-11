a0090de83c24878e5a0dafc7a60f5622e4c255bd
Naladevanka D. <nalaajei@gmail.com>
2026-03-11 15:31:44 +0700

    change mongodb

--- configurations/mongoConnection.js
+++ configurations/mongoConnection.js
@@ -1,9 +1,7 @@
-const mongoose = require('mongoose');
-const { setServers } = require ("node:dns").promises;
+const mongoose = require("mongoose");
+const { setServers } = require("node:dns").promises;
 setServers(["1.1.1.1", "8.8.8.8"]);
 
-mongoose
-	.connect("mongodb+srv://nalaajei:nala123@cluster0.6qdderb.mongodb.net/test")
-	.then(() => console.log("Connected!"));
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected!"));
 
 module.exports = mongoose;
